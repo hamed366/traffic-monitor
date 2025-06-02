6db65175ed751a8da2236a1c0c863b2033fd99bb
hamed366 <82051937+hamed366@users.noreply.github.com>
2025-06-02 13:12:23 +0800

    Update check_traffic_monitor.py

--- check_traffic_monitor.py
+++ check_traffic_monitor.py
@@ -2,22 +2,22 @@ from telegram import Bot
 import psutil
 import socket
 import datetime
+import asyncio
 
-# مقادیری که توسط setup.sh به صورت مستقیم در این فایل نوشته می‌شوند:
 TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 CHAT_ID = "YOUR_CHAT_ID"
 INTERFACE = "YOUR_NETWORK_INTERFACE"
-THRESHOLD_MB = 500  # مثلا 500 مگ
+THRESHOLD_MB = 500
 SERVER_NAME = "YOUR_SERVER_NAME"
 
-def send_message(message):
+async def send_message(message):
     bot = Bot(token=TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text=message)
+    await bot.send_message(chat_id=CHAT_ID, text=message)
 
-def check_traffic():
+async def check_traffic():
     counters = psutil.net_io_counters(pernic=True)
     if INTERFACE not in counters:
-        send_message(f"⚠️ Interface '{INTERFACE}' یافت نشد.")
+        await send_message(f"⚠️ Interface '{INTERFACE}' یافت نشد.")
         return
 
     data = counters[INTERFACE]
@@ -27,7 +27,7 @@ def check_traffic():
     message = f"📡 [{SERVER_NAME}] ترافیک مصرفی در {timestamp}:\n🔻 مصرف کل: {used_mb:.2f} MB"
     if used_mb > THRESHOLD_MB:
         message += f"\n🚨 هشدار: مصرف بیش از {THRESHOLD_MB}MB!"
-    send_message(message)
+    await send_message(message)
 
 if __name__ == "__main__":
-    check_traffic()
+    asyncio.run(check_traffic())
